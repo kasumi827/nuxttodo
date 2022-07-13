7a1ae3716a80ffacbe1789b85755cefdee602a6e
kasumi827 <kasumiuchan827@gmail.com>
2022-07-13 19:58:07 +0900

    first commit

---
+++ plugins/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(
+        {
+            apiKey: "AIzaSyBlxihdj0jeKdhKBd8w_b5t3DxJO2ZAMXo",
+            authDomain: "nuxttodo-4b83d.firebaseapp.com",
+            databaseURL: "https://nuxttodo-4b83d-default-rtdb.firebaseio.com",
+            projectId: "nuxttodo-4b83d",
+            storageBucket: "nuxttodo-4b83d.appspot.com",
+            messagingSenderId: "22077865415",
+            appId: "1:22077865415:web:4592130afe395bbe2c6de2",
+            measurementId: "G-EFFPRB5GY9"
+        }
+    )
+}
